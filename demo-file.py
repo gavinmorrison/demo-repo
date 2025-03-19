6ca99b2b1c0c97962fec9b1c032280b884ab3987
Gavin Morrison <gavinmorrison@users.noreply.github.com>
2025-03-19 14:24:54 +0000

    Create demo-file.py

---
+++ demo-file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def sum_of_evens(numbers):
+    total = 0
+    for num in numbers:
+        if num % 2:
+            total += num
+    return total
+
+# Test the function
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Sum of even numbers:", sum_of_evens(numbers))
